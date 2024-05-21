2be9830ee34ae9cbda91e9c93fe58b3068b7bb00
SUBHIN-TM <subhintm2018@gmail.com>
2024-05-21 18:52:03 +0530

    covered primitve and basic

---
+++ variableMe.js
@@ -0,0 +1,12 @@
+//STRING
+var greetings = "hello subhin";
+greetings = "HI SURYA";
+greetings.toUpperCase();
+console.log(greetings);
+//NUMBER
+var userId = 3344;
+console.log(userId);
+//BOOLEAN
+var isLogged = true;
+if (isLogged)
+    console.log("true");
--- variableMe.ts
+++ variableMe.ts
@@ -1,1 +1,22 @@
+//STRING
 let greetings:string="hello subhin"
+greetings="HI SURYA"
+greetings.toUpperCase()
+console.log(greetings)
+
+
+//NUMBER
+let userId:number=3344
+console.log(userId);
+
+
+
+//BOOLEAN
+let isLogged:boolean=true
+if(isLogged) console.log("true");
+
+
+//ANY
+let automatic; //BY DEFAULT IT MADE AN ASSIGNMENT OF ANY
+let manual :any;  //BOTH ARE EQUAL, NOT A GOOD PRACTICE TO USE ANY
+
