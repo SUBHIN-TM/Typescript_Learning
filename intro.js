d0785a52a97be6a300f00b7395664d96552735ea
SUBHIN-TM <subhintm2018@gmail.com>
2024-05-21 17:59:25 +0530

    initial

---
+++ intro.js
@@ -0,0 +1,4 @@
+var user = { name: "subhin", age: 28 };
+console.log("hi");
+console.log(user.name);
+
---
+++ intro.ts
@@ -0,0 +1,5 @@
+
+let user={name:"subhin",age:28}
+console.log("hi")
+console.log(user.name)
+console.log(user.mail); 
