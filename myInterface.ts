1db7e6ad488a7e5df287d8d7fcbbe28ec12cb59b
SUBHIN-TM <subhintm2018@gmail.com>
2024-05-27 12:43:28 +0530

    interface and extends

--- myInterface.ts
+++ myInterface.ts
@@ -2,12 +2,34 @@ interface Users{
     name:string,
     id:number,
     startTrial:()=> string //FUNCTION RETURN TYPE SHOULD BE STRING
-    getCoupon (couponName:string,value:number): number
+    getCoupon (couponName:string,value:number): number  //ANOTHER MODEL OF INITIALIZING A FUNCTION
 }
 
+interface Users{
+    email:string
+}
+
+
+
 const sample:Users={
+    email:'shjyb@gmial..com',
     name:"owner",
     id:1,
     startTrial:()=> "returned a string",
     getCoupon:(couponName:'offer',value:10)=>{ return 1
 }}
+
+
+interface Admins extends Users{  //ADMIN INTERFACE INCLUDED USERS DATA TYPES
+role:string
+}//EXTENDAS USED TO HAVE AN ACCESS OF THAT INSTANCE DATA TYPE
+
+const admins:Admins={
+    email:'shjyb@gmial..com',
+    name:"owner",
+    id:1,
+    startTrial:()=> "returned a string",
+    getCoupon:(couponName:'offer',value:10)=>{ return 1
+},
+role:'admin'
+}
